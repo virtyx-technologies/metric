401043e5210d9389a7e452e841cceac2b910327a
Ethan Mick <ethan@ethanmick.com>
2018-07-19 10:10:16 -0400

    Metrics, Metadata, new format

---
+++ metric.go
@@ -0,0 +1,85 @@
+// Virtyx Metrics
+//
+// Virtyx records metrics in a simple format of name, value, with optional tags
+// being added.
+package metric
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type (
+	// The struct returned to the agent by a plugin,
+	// containing all metrics and metadata and the
+	// text of any error which may have occurred
+	Response struct {
+		Error     string      `json:"error"`
+		Metrics   []*Metric   `json:"metrics"`
+		Metadatas []*Metadata `json:"metadata"`
+	}
+
+	// Encapsulates a metric captured by a plugin
+	Metric struct {
+		Name  string                 `json:"name"`  // Metric name
+		Value float64                `json:"value"` // Metric value
+		Tags  map[string]interface{} `json:"tags"`  // Optional tags that can be used for searching
+	}
+
+	// Encapsulates metadata (textual information) captured by a plugin
+	Metadata struct {
+		Name string `json:"type"` // The metadata name
+		Data string `json:"data"` // The data captured
+	}
+)
+
+// Add a metric to the response
+func (r *Response) Metric(m *Metric) {
+	r.Metrics = append(r.Metrics, m)
+}
+
+// Helper function for recording a value
+//  response.Value("system.cpu.percent", 23.32, nil)
+func (r *Response) Value(name string, val float64, tags map[string]interface{}) {
+	r.Metrics = append(r.Metrics, &Metric{Name: name, Value: val, Tags: tags})
+}
+
+// Add metadata to the response
+func (r *Response) Metadata(m *Metadata) {
+	r.Metadatas = append(r.Metadatas, m)
+}
+
+// Add metadata to the response
+func (r *Response) Data(name string, data string) {
+	r.Metadatas = append(r.Metadatas, &Metadata{Name: name, Data: data})
+}
+
+func (r *Response) FindMetric(name string) *Metric {
+	for _, m := range r.Metrics {
+		if m.Name == name {
+			return m
+		}
+	}
+	return nil
+}
+
+func (r *Response) FindMetadata(name string) *Metadata {
+	for _, m := range r.Metadatas {
+		if m.Name == name {
+			return m
+		}
+	}
+	return nil
+}
+
+func (m *Metric) String() string {
+	tags, err := json.Marshal(m.Tags)
+	if err != nil {
+		tags = []byte("{}")
+	}
+	return fmt.Sprintf("%s|%f|%s", m.Name, m.Value, string(tags))
+}
+
+func (m *Metadata) String() string {
+	return fmt.Sprintf("%s|%s", m.Name, m.Data)
+}
---
+++ metric_test.go
@@ -0,0 +1,88 @@
+package metric
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetricString(t *testing.T) {
+	metric := &Metric{Name: "system.cpu.percent", Value: 1, Tags: nil}
+	assert.Equal(t, "system.cpu.percent|1.000000|null", metric.String())
+}
+
+func TestMetadataString(t *testing.T) {
+	meta := &Metadata{Name: "metadata", Data: "data"}
+	assert.Equal(t, "metadata|data", meta.String())
+}
+
+func TestAddMetric(t *testing.T) {
+	a := assert.New(t)
+	r := &Response{}
+
+	a.Equal(0, len(r.Metrics))
+	metric := &Metric{Name: "system.cpu.percent", Value: 1, Tags: nil}
+	r.Metric(metric)
+	a.Equal(1, len(r.Metrics))
+	a.Equal(metric, r.Metrics[0])
+}
+
+func TestValue(t *testing.T) {
+	a := assert.New(t)
+	r := &Response{}
+	r.Value("system.cpu.percent", 1, nil)
+	a.Equal(1, len(r.Metrics))
+	a.Equal("system.cpu.percent", r.Metrics[0].Name)
+	a.Equal(float64(1), r.Metrics[0].Value)
+	a.Nil(r.Metrics[0].Tags)
+}
+
+func TestAddMetadata(t *testing.T) {
+	a := assert.New(t)
+	r := &Response{}
+
+	a.Equal(0, len(r.Metadatas))
+	metadata := &Metadata{Name: "metadata", Data: "unqualified"}
+	r.Metadata(metadata)
+	a.Equal(1, len(r.Metadatas))
+	a.Equal(metadata, r.Metadatas[0])
+}
+
+func TestData(t *testing.T) {
+	a := assert.New(t)
+	r := &Response{}
+
+	r.Data("metadata", "data")
+	a.Equal(1, len(r.Metadatas))
+	a.Equal("metadata", r.Metadatas[0].Name)
+	a.Equal("data", r.Metadatas[0].Data)
+}
+
+func TestFindMetric(t *testing.T) {
+	a := assert.New(t)
+	r := &Response{}
+
+	a.Equal(0, len(r.Metrics))
+	m1 := &Metric{Name: "metric.one", Value: 0, Tags: nil}
+	r.Metric(m1)
+	m2 := &Metric{Name: "metric.two", Value: 1, Tags: nil}
+	r.Metric(m2)
+	found := r.FindMetric("metric.one")
+	a.Equal(m1, found)
+	notFound := r.FindMetric("metric.three")
+	a.Nil(notFound)
+}
+
+func TestFindMetadata(t *testing.T) {
+	a := assert.New(t)
+	r := &Response{}
+
+	m1 := &Metadata{Name: "metadata.one", Data: "321"}
+	r.Metadata(m1)
+	m2 := &Metadata{Name: "metadata.two", Data: "xyz"}
+	r.Metadata(m2)
+	found := r.FindMetadata("metadata.one")
+	a.Equal(m1, found)
+	notFound := r.FindMetadata("metadata.three")
+	a.Nil(notFound)
+}
